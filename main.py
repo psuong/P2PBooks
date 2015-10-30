c25afb8ff5748bf4bf8d2d40c660817b85552e50
porrith <psuong95@gmail.com>
2015-10-30 16:02:06 -0400

    Base files for Ui_FormLogin added.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 from PySide import QtGui
-from views.main_view import UploadFormView
+from views.main_view import UploadFormView, LoginFormView
 
 
 class App(QtGui.QApplication):
     def __init__(self, sys_argv):
         super(App, self).__init__(sys_argv)
-        self.main_view = UploadFormView(None)
+        self.main_view = LoginFormView(None)
         self.main_view.show()
 
 if __name__ == '__main__':
--- views/main_view.py
+++ views/main_view.py
@@ -1,5 +1,5 @@
 from PySide import QtGui
-from ui import Ui_UploadForm
+from ui import Ui_UploadForm, Ui_LoginForm
 
 
 class UploadFormView(QtGui.QWidget):
@@ -20,3 +20,21 @@ class UploadFormView(QtGui.QWidget):
 
     def submit(self):
         self.ui.preview_text_edit.setText('P SUONG')
+
+class LoginFormView(QtGui.QWidget):
+    def __init__(self, model):
+        self.model = model
+        super(LoginFormView, self).__init__()
+        self.ui = Ui_LoginForm.Ui_Form()
+        self.build_ui()
+
+    def build_ui(self):
+        self.ui.setupUi(self)
+        # Connect buttons to login
+        self.ui.login_push_button.clicked.connect(self.login)
+
+    def login(self):
+        # Grab component in object
+        username = self.ui.username_line_edit.text()
+        if username:
+            self.ui.username_line_edit.setText("We good." + username)
