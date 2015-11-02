5f5ead85f469485a5928b52c0f7e72eb24f2260f
fioger <fioger95@live.com>
2015-11-02 13:21:17 -0500

    RegisterFormView created

--- views/main_view.py
+++ views/main_view.py
@@ -1,5 +1,5 @@
 from PySide import QtGui
-from ui import Ui_UploadForm
+from ui import Ui_UploadForm, Ui_RegisterForm
 
 
 class UploadFormView(QtGui.QWidget):
@@ -20,3 +20,20 @@ class UploadFormView(QtGui.QWidget):
 
     def submit(self):
         self.ui.preview_text_edit.setText('P SUONG')
+
+
+class RegisterFormView(QtGui.QWidget):
+    def __init__(self, model):
+        self.model = model
+        super(RegisterFormView, self).__init__()
+        self.ui = Ui_RegisterForm.Ui_Form()
+        self.build_ui()
+
+    def build_ui(self):
+        self.ui.setupUi(self)
+
+        # Connect buttons to functions
+        self.ui.submit_push_button.clicked.connect(self.submit)
+
+    def submit(self):
+        pass
