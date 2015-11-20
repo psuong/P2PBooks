7fcbb448a4223f166373fdccb96be3c1c001cd37
exp0nge <mdislamwork@gmail.com>
2015-11-20 14:48:59 -0500

    Merged Registered MainWindow

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 from PySide import QtGui
-from views.main_view import UploadFormView, MainWindowVisitorView, ReaderFormView
+from views.main_view import UploadFormView, MainWindowVisitorView, ReaderFormView, MainWindowRegisteredView
 
 
 class App(QtGui.QApplication):
     def __init__(self, sys_argv):
         super(App, self).__init__(sys_argv)
-        self.main_view = MainWindowVisitorView(None)
+        self.main_view = MainWindowRegisteredView(None)
         self.main_view.show()
 
 if __name__ == '__main__':
