fc5a49c55d6ff471a3963291794b21397e6f71ed
exp0nge <mdislamwork@gmail.com>
2015-11-21 22:39:03 -0500

    Merged Fioger's registered main window formatting fixes and the ApprovalList UI

--- views/ui/Ui_MainWindowRegistered.py
+++ views/ui/Ui_MainWindowRegistered.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'designer/Ui_MainWindowRegistered.ui'
+# Form implementation generated from reading ui file 'designer\Ui_MainWindowRegistered.ui'
 #
-# Created: Sat Nov 21 17:28:11 2015
-#      by: pyside-uic 0.2.15 running on PySide 1.2.4
+# Created: Sat Nov 21 22:36:33 2015
+#      by: pyside-uic 0.2.15 running on PySide 1.2.2
 #
 # WARNING! All changes made in this file will be lost!
 
