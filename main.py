4b501c290cda57cdbe7daf1c481251de382ad0ac
Christopher Yang <cyang001@citymail.cuny.edu>
2015-11-25 17:52:58 -0500

    Merge with read-ebook

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from models import main_model
 class App(QtGui.QApplication):
     def __init__(self, sys_argv):
         super(App, self).__init__(sys_argv)
-        self.main_view = ReportDialogView(main_model)
-        # self.main_view = MainWindowVisitorView(main_model)
+        # self.main_view = ReportDialogView(main_model)
+        self.main_view = MainWindowVisitorView(main_model)
         self.main_view.show()
 
 if __name__ == '__main__':
