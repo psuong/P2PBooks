51faded8c115301fbc0692808dd3bffeac05719c
Fioger <fioger95@Live.com>
2015-11-28 19:22:16 -0500

    Changed user_check functionality to include case insensitivity

--- models/main_model.py
+++ models/main_model.py
@@ -268,6 +268,6 @@ def review_exists(reviewer, book_instance):
 # Checks if user exists
 def user_exists(username):
     for pickle in os.listdir(ACCOUNT_DIR_PATH):
-        if pickle == username.lower() + ".pickle":
+        if pickle.lower() == username.lower() + ".pickle":
             return True
     return False
